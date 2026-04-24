a00994731df633cc4118ca063d5f74f34a3c505b
Alejandro Higuita Soto <hahiguit@gmail.com>
2026-04-23 23:00:33 -0500

    fix: EFS SG usa CIDR de VPC en lugar de SG reference para evitar ciclo y error de mount

--- infra/modules/efs/variables.tf
+++ infra/modules/efs/variables.tf
@@ -1,4 +1,5 @@
 variable "environment"        { type = string }
 variable "vpc_id"             { type = string }
+variable "vpc_cidr"           { type = string }
 variable "private_subnet_ids" { type = list(string) }
 variable "ecs_sg_id"          { type = string }
